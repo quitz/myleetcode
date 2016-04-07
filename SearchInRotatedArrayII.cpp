4df1d0911152a1c5c932bfd9b4a8a7f12227b9e6
quitz <quitz19930619@gmail.com>
2016-04-07 18:14:46 +0800

    SearchInRotatedArrayII

---
+++ SearchInRotatedArrayII.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int start =0, end = nums.size()-1;
+        while(start<=end) {
+            int mid = (start + end)/2;
+            if(nums[mid]==target) return true;  
+            
+            if(nums[mid]>nums[end]) { // left half sorted
+                if(target>=nums[start] && target<nums[mid])
+                    end = mid-1;
+                else
+                    start = mid+1;
+            }
+            else if( nums[mid] < nums[end]){  // right half sorted
+            
+                if(target>nums[mid] && target<=nums[end] ) 
+                    start = mid+1;
+                else
+                    end = mid-1;
+            }
+            else
+                end--;
+        }
+        return false;
+    }
+};

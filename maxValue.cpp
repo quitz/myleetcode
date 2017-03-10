1aed37d74dd058b69d12934c5e0c55bc489ce397
quitz <quitz19930619@gmail.com>
2017-03-09 22:06:52 -0800

    the way to Google

---
+++ maxValue.cpp
@@ -0,0 +1,48 @@
+struct Iteminfo{
+    string name;
+    int value;
+    int maximum_stack_size;
+};
+
+class solution{
+    public:
+        int maxValue(int n, vector<string> obj, vector<Iteminfo> item){
+            int ret = 0;
+            map<string,int> mp;
+            map<string,int> val;
+            map<string,int> limit;
+            
+            for(auto i: obj)
+                mp[i]++;
+
+            for(auto i:item){
+                val[i.name] = i.value;
+                limit[i.name] = i.maximum_stack_size(); 
+            }
+
+            priority_queue<int> pq;
+            for(auto it = mp.begin();it!= mp.end();it++){
+                string na = it->first;
+                int num = it->second;
+                int max_size = limit[na];
+                int v = val[na];
+
+                int times = num / max_size;
+                int left = num % max_size;
+                for(int j=0;j<times;j++)
+                    pq.push(v*max_size);
+                if(left!=0)
+                    pq.push(v*left);
+
+            }
+
+            for(int i=1;i<=k;i++){
+                while(!pq.empty()){
+                    ret += pq.top();
+                    pq.pop();
+                }
+            }
+
+            return ret;
+        }
+}

ffbedb41bd0ee3511092bd35a262eb0f224e080d
quitz <quitz19930619@gmail.com>
2017-03-09 20:59:07 -0800

    the way to Google

---
+++ findLeaves.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+private:
+    int dfs(TreeNode* root, vector<vector<int>>& res){
+        if(!root) return 0;
+        int level = max(dfs(root->left, res), dfs(root->right, res)) + 1;
+        if(level > (int)res.size()) res.push_back(vector<int>());
+        res[level - 1].push_back(root->val);
+        return level;
+    }
+public:
+    vector<vector<int>> findLeaves(TreeNode* root) {
+        vector<vector<int>> res;
+        dfs(root, res);
+        return res;
+    }
+};

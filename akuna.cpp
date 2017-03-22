af2077e9c365a2c1c79459a86e7b6f0e69a76923
quitz <quitz19930619@gmail.com>
2017-03-21 23:50:43 -0700

    the way to Google

--- akuna.cpp
+++ akuna.cpp
@@ -1,6 +1,7 @@
 
 // implement the Comparable template here
 template<typename Derived> struct Comparable{
+template<typename Derived> struct Comparable{
     bool operator==(const Comparable<Derived>& o1) const noexcept{
         const Derived& d1 = static_cast<const Derived*>(o1);
         const Derived& d2 = static_cast<const Derived*>(*this);
